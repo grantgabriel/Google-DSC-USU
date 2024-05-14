f128a247284301e743ed51985fbd14c466ea28a2
M-Thoriq <mthoriqalasyjari@gmail.com>
2024-05-14 21:23:25 +0700

    Fix: fix components' src path

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,7 +2,7 @@
 <nav class="w-[100dvw] h-fit py-5 border px-5 lg:px-48">
     <div class="flex justify-between my-auto">
         <div>
-            <img src="assets/img/google.svg" class="h-7" alt="">
+            <img src="img/google.svg" class="h-7" alt="">
         </div>
         <p class="font-semibold my-auto hidden">Google Developer Students Club</p>
         <nav class="fixed lg:static h-fit z-10 p-4 lg:p-0 border-t lg:border-0 w-screen lg:w-fit bottom-0 left-0">
