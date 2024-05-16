368728ba186e41e8e863bffad9220077461fc155
M-Thoriq <mthoriqalasyjari@gmail.com>
2024-05-16 21:50:04 +0700

    Add index/landing page for gdsc USU

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 use Carbon\Carbon;
 class EventController extends Controller
 {
     public function index(){
         $events = Event::get();
+        $userCount = count(User::get());
 
-        return view('index', compact('events'));
+        return view('index', compact(['events', 'userCount']));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,76 @@
 
 @section('content')
 
-<section id="home" class="bg-gdsc bg-center py-36">
-    <div class="grid justify-center gap-5 items-center text-center">    
-        <h1 class="text-5xl font-semibold">Google Developer Student Clubs</h1>
-        <p class="max-w-screen-lg mx-auto text-xl">Google Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a GDSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.</p>
+<section id="home" class="grid gap-5 w-full p-2  overflow-x-hidden">
+    <div class="flex justify-center items-center w-full h-full">
+        <div class="w-fit lg:w-full">    
+            <img class="rounded-lg lg:object-cover lg:max-h-52 lg:min-w-full" src="img/bg-USU.png" alt="">
+        </div>
+    </div>
+    <div class="lg:px-24 text-slate-700 grid gap-5">
+        <h1 class="font-bold text-3xl lg:text-4xl text-slate-900">Universitas Sumatera Utara - Medan, Indonesia</h1>
+        <p class="text-lg"><span class="p-1 bg-slate-50 rounded-md border">{{ $userCount }}</span> Group Members</p>
+    </div>
+    <div class="bg-gradient-to-r from-white via-gray-200 to-white py-2 flex justify-center">
+        <button class="bg-blue-600 text-white rounded-md lg:px-8 lg:py-2 lg:w-fit py-2 text-lg font-semibold">
+            Join Us
+        </button>
+    </div>
+    <div class="lg:px-24 rounded-md">
+        <div class="rounded-md grid gap-3">
+            <h1 class="font-semibold text-4xl">Upcoming Events</h1>
+            <div class="border bg-slate-50 rounded-md grid grid-cols-1 lg:grid-cols-4 px-6 py-8 gap-4">
+            @foreach ($events as $item)
+            @if($item->time > now())
+                <div class="flex flex-col gap-2 justify-center items-center max-w-[25ch] text-lg">
+                    <img class="w-44 h-44 rounded-full bg-gray-800" src="{{ $item->event_profile }}">
+                    <span class="font-light">{{ Carbon\Carbon::parse($item->time)->format('M d, Y') }}</span>
+                    <p class="font-semibold text-center underline">{{ $item->event_name }}</p>
+                    
+                    @if ($item->type == 'Hybrid')
+                        <span class="text-amber-600 bg-amber-100 rounded-lg px-2 py-1 text-center">
+                    @elseif ($item->type == 'Online')
+                        <span class="text-green-700 bg-green-100 rounded-lg px-2 py-1 text-center">
+                    @else
+                        <span class="text-blue-600 bg-slate-200 rounded-lg px-2 py-1 text-center">   
+                    @endif
+                        
+                        {{ $item->type }}
+                    </span>
+                    <p class="text-gray-600 text-center">
+                        Universitas Sumatera Utara - Medan, Indonesia
+                    </p>
+                </div>
+            @endif
+            @endforeach
+            </div>
+            <h1 class="font-semibold text-4xl">Past Events</h1>
+            <div class="border bg-slate-50 rounded-md grid grid-cols-1 lg:grid-cols-4 px-6 py-8 gap-4">
+            @foreach ($events as $item)
+            @if($item->time < now())
+                <div class="flex flex-col gap-2 justify-center items-center max-w-[25ch] text-lg">
+                    <img class="w-44 h-44 rounded-full bg-gray-800" src="{{ $item->event_profile }}">
+                    <span class="font-light">{{ Carbon\Carbon::parse($item->time)->format('M d, Y') }}</span>
+                    <p class="font-semibold text-center underline">{{ $item->event_name }}</p>
+                    
+                    @if ($item->type == 'Hybrid')
+                        <span class="text-amber-600 bg-amber-100 rounded-lg px-2 py-1 text-center">
+                    @elseif ($item->type == 'Online')
+                        <span class="text-green-700 bg-green-100 rounded-lg px-2 py-1 text-center">
+                    @else
+                        <span class="text-blue-600 bg-slate-200 rounded-lg px-2 py-1 text-center">   
+                    @endif
+                        
+                        {{ $item->type }}
+                    </span>
+                    <p class="text-gray-600 text-center">
+                        Universitas Sumatera Utara - Medan, Indonesia
+                    </p>
+                </div>
+            @endif
+            @endforeach
+            </div>
+        </div>
     </div>
 </section>
 
