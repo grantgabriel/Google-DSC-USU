1ed500d75c1846f9b46681292548bbeea69b6075
M-Thoriq <mthoriqalasyjari@gmail.com>
2024-05-18 20:37:57 +0700

    Fix: fix navbar colliding

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,27 +1,27 @@
-<div class="border-t-[1px] mt-8">
+<div class="border-t-[1px] mt-8 pb-12 lg:pb-0">
     <div class=" mx-8 md:mx-20 lg:mx-[200px]">
         <div class="lg:grid lg:grid-flow-col lg:justify-stretch">
-            <img src="img/gdsc.svg" class="object-contain w-60 mt-12" alt="">
+            <img src="{{ asset('img/gdsc.svg') }}" class="object-contain w-60 mt-12 lg:mx-0 mx-auto" alt="">
             <div>
-                <h5 class="text-[#676c72] mt-6 lg:mt-14">Quick Links</h5>
-                <ul class="list-inside list-['>']">
-                    <a href=""><li class="mt-3 text-sm hover:underline underline-offset-4">&nbsp; Chapters</li></a>
-                    <a href=""><li class=" text-sm hover:underline underline-offset-4">&nbsp; Upcoming events</li></a>
-                    <a href=""><li class=" text-sm hover:underline underline-offset-4">&nbsp; About GDSC</li></a>
-                    <a href=""><li class=" text-sm hover:underline underline-offset-4">&nbsp; Lead Terms</li></a></a>
-                    <a href=""><li class=" text-sm hover:underline underline-offset-4">&nbsp; Participation Terms</li></a>
-                    <a href=""><li class=" text-sm hover:underline underline-offset-4">&nbsp; Privacy</li></a>
-                    <a href=""><li class=" text-sm hover:underline underline-offset-4">&nbsp; Terms</li></a>
+                <h5 class="text-[#676c72] text-lg lg:text-base mt-6 lg:mt-14">Quick Links</h5>
+                <ul class="list-inside mt-3 list-['>']">
+                    <a href=""><li class="lg:text-sm text-lg hover:underline underline-offset-4">&nbsp; Chapters</li></a>
+                    <a href=""><li class="lg:text-sm text-lg hover:underline underline-offset-4">&nbsp; Upcoming events</li></a>
+                    <a href=""><li class="lg:text-sm text-lg hover:underline underline-offset-4">&nbsp; About GDSC</li></a>
+                    <a href=""><li class="lg:text-sm text-lg hover:underline underline-offset-4">&nbsp; Lead Terms</li></a></a>
+                    <a href=""><li class="lg:text-sm text-lg hover:underline underline-offset-4">&nbsp; Participation Terms</li></a>
+                    <a href=""><li class="lg:text-sm text-lg hover:underline underline-offset-4">&nbsp; Privacy</li></a>
+                    <a href=""><li class="lg:text-sm text-lg hover:underline underline-offset-4">&nbsp; Terms</li></a>
                 </ul>
             </div>
             <div>
-                <h5 class="text-[#676c72] mt-14 mb-4">Social</h5>
+                <h5 class="text-[#676c72] text-lg lg:text-base mt-14 mb-4">Social</h5>
                 <div class="justify-start flex mb-20">
-                    <a href="https://twitter.com/googledevs"><img src="/img/twitter.jpeg" class="object-contain mr-5 w-[14px]" alt=""></a>
-                    <a href="https://www.facebook.com/googlefordevs"><img src="/img/facebook.svg" class="object-contain mr-5 w-[14px]" alt=""></a>
-                    <a href="https://www.instagram.com/googlefordevs/"><img src="/img/instagram.svg" class="object-contain mr-5 w-[14px]" alt=""></a>
-                    <a href="https://www.linkedin.com/company/googledevelopers/"><img src="/img/linkedin.svg" class="object-contain mr-[22px] w-[14px]" alt=""></a>
-                    <a href="https://www.youtube.com/googledevelopers"><img src="/img/youtube.svg" class="object-contain mr-5 w-[14px]" alt=""></a>
+                    <a href="https://twitter.com/googledevs"><img src="/img/twitter.jpeg" class="object-contain mr-5 w-[24px] " alt=""></a>
+                    <a href="https://www.facebook.com/googlefordevs"><img src="/img/facebook.svg" class="object-contain mr-5 w-[24px] " alt=""></a>
+                    <a href="https://www.instagram.com/googlefordevs/"><img src="/img/instagram.svg" class="object-contain mr-5 w-[24px] " alt=""></a>
+                    <a href="https://www.linkedin.com/company/googledevelopers/"><img src="/img/linkedin.svg" class="object-contain mr-[22px] w-[24px] " alt=""></a>
+                    <a href="https://www.youtube.com/googledevelopers"><img src="/img/youtube.svg" class="object-contain mr-5 w-[24px] " alt=""></a>
                 </div>
             </div>   
         </div>       
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,10 +4,10 @@
 <nav class="w-full h-fit py-4 border px-5 lg:px-48 border-slate-300">
     <div class="flex items-center justify-between my-auto">
         <a href="{{route('home')}}">
-            <img src="https://res.cloudinary.com/startup-grind/image/upload/dpr_2.0,fl_sanitize/v1/gcs/platform-data-dsc/contentbuilder/logo_dark_horizontal_097s7oa.svg" class="h-6" alt="">
+            <img src="https://res.cloudinary.com/startup-grind/image/upload/dpr_2.0,fl_sanitize/v1/gcs/platform-data-dsc/contentbuilder/logo_dark_horizontal_097s7oa.svg" class="lg:h-6 h-4" alt="">
         </a>
-        <nav class=" fixed lg:static h-fit z-10 p-4 lg:p-0 border-t lg:border-0 w-screen lg:w-fit bottom-0 left-0">
-            <div class="flex lg:justify-evenly gap-5 my-auto w-full lg:w-fit text-slate-600">
+        <nav class="bg-white fixed lg:static h-fit z-10 p-4 lg:p-0 border-t lg:border-0 w-screen lg:w-fit bottom-0 left-0">
+            <div class="flex justify-center lg:justify-evenly gap-5 my-auto w-full lg:w-fit text-slate-600">
                 <a class="hover:text-black hover:bg-gray-100 rounded-lg active:bg-gray-200">
                     <p class="">About GDSC</p>
                 </a>
@@ -19,14 +19,7 @@
                 </a>
                 
             </div>
-        </nav>
-
-
-        
-            
-        
-            
-        
+        </nav>         
 
 
         @auth
