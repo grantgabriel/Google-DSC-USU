9a2d6fc59bfaa1e78c000d723c635ab9b63c4212
RafiDevari <rafidevari2004@gmail.com>
2024-05-27 22:23:52 +0700

    ADD ALL STATISTIC

--- resources/views/admin/event-stat.blade.php
+++ resources/views/admin/event-stat.blade.php
@@ -29,36 +29,159 @@
 
 
                 <h1>Event Ratings Chart</h1>
-                <canvas id="ratingsChart" width="400" height="200"></canvas>
+                <canvas id="ratings"></canvas>
+
+                <h1>Speaker Ratings Chart</h1>
+                <canvas id="ratingspeak"></canvas>
+
+                <h1>Persentasi Chart</h1>
+                <canvas id="presensi"></canvas>
+
+
                 
 
             </div>
         </div>
 
+        
         <script>
-            const ctx = document.getElementById('ratingsChart').getContext('2d');
-            const ratingsChart = new Chart(ctx, {
-                type: 'line',
+            // Get the context of the canvas element we want to select
+            var ctx = document.getElementById('ratings').getContext('2d');
+    
+            // Create a new Chart object
+            var ratings = new Chart(ctx, {
+                type: 'bar',
                 data: {
-                    labels: @json($dates),
+                    labels: ['1 Star', '2 Star', '3 Star', '4 Star', '5 Star'],
                     datasets: [{
-                        label: 'Average Rating',
-                        data: @json($averageRatings),
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        label: 'Number of Rating',
+                        data: [
+                            {{$event->rsvp()->whereNotNull('rating')->where('rating','1')->count()}}, 
+                            {{$event->rsvp()->whereNotNull('rating')->where('rating','2')->count()}}, 
+                            {{$event->rsvp()->whereNotNull('rating')->where('rating','3')->count()}}, 
+                            {{$event->rsvp()->whereNotNull('rating')->where('rating','4')->count()}}, 
+                            {{$event->rsvp()->whereNotNull('rating')->where('rating','5')->count()}}
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
                         borderWidth: 1
                     }]
                 },
                 options: {
                     scales: {
                         y: {
-                            beginAtZero: true,
-                            suggestedMax: 5
+                            beginAtZero: true
                         }
                     }
                 }
             });
         </script>
 
-        
+
+        <script>
+            // Get the context of the canvas element we want to select
+            var ctx = document.getElementById('ratingspeak').getContext('2d');
+
+            // Create a new Chart object
+            var ratings = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['1 Star', '2 Star', '3 Star', '4 Star', '5 Star'],
+                    datasets: [{
+                        label: 'Number of Rating',
+                        data: [
+                            {{$event->rsvp()->whereNotNull('speaker_rating')->where('speaker_rating','1')->count()}}, 
+                            {{$event->rsvp()->whereNotNull('speaker_rating')->where('speaker_rating','2')->count()}}, 
+                            {{$event->rsvp()->whereNotNull('speaker_rating')->where('speaker_rating','3')->count()}}, 
+                            {{$event->rsvp()->whereNotNull('speaker_rating')->where('speaker_rating','4')->count()}}, 
+                            {{$event->rsvp()->whereNotNull('speaker_rating')->where('speaker_rating','5')->count()}}
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        </script>
+
+
+        <script>
+            // Get the context of the canvas element we want to select
+            var ctx = document.getElementById('presensi').getContext('2d');
+
+            // Create a new Chart object
+            var ratings = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: ['Total RSVP', 'Attended'],
+                    datasets: [{
+                        label: 'Number of Rating',
+                        data: [
+                            {{$event->rsvp()->count()}}, 
+                            {{$event->rsvp()->where('attendance_detail','Attend')->count()}}
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        </script>
+
 @endsection
