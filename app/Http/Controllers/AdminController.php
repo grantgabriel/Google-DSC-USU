5a9bd399607154c6cd8a2b6470a5f5313cfa5701
RafiDevari <rafidevari2004@gmail.com>
2024-05-28 10:51:18 +0700

    nambah review

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -30,7 +30,7 @@ class AdminController extends Controller
     }
 
     public function eventsurvey($id){
-        $event = Event::find($id);
+        $event = Rsvp::where('event_id',$id)->whereNotNull('rating')->get();
         return view('admin.event-survey',compact('event'));
     }
 
--- resources/views/admin/event-survey.blade.php
+++ resources/views/admin/event-survey.blade.php
@@ -1,5 +1,24 @@
 @extends('layout.header_admin')
 
 @section('content_warning')
-        PERMULAANzz HEBH@EHK@
+        <div class="flex justify-center">
+                <div class="flex-col">
+                        <table>
+                                <thead>
+                                        <tr>
+                                                <th>Score</th>
+                                                <th>Suggestion</th>
+                                        </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($event as $item)
+                                        <tr>
+                                                <td>{{$item->rating}}</td>
+                                                <td>{{$item->review}}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                        </table>
+                </div>
+        </div>
 @endsection
