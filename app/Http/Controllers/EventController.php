cd37f227f34feb413638502ce619e5fa1344e9de
RafiDevari <rafidevari2004@gmail.com>
2024-06-02 09:15:00 +0700

    Fix attendance,default could not attend

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -32,7 +32,8 @@ class EventController extends Controller
         $slug = Str::slug($event->event_name);
         Rsvp::create([
             'event_id' => $request->eventid,
-            'user_id' => $request->userid
+            'user_id' => $request->userid,
+            'attendance_detail' => 'Could Not Attend'
         ]);
 
         return redirect('/event/'.$ev_id.'-'.$slug);
