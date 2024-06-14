9d77344590194e651ac799ca8a8d8a9581ed2a5e
IbraRizqy <rizqyibra@gmail.com>
2024-06-14 15:34:41 +0700

    fix : modal text

--- resources/views/admin/event-detail.blade.php
+++ resources/views/admin/event-detail.blade.php
@@ -5,7 +5,7 @@
 <div id="logoutModal1" class="hidden fixed inset-0 bg-black bg-opacity-70 z-50 flex items-center justify-center">
     <div class="bg-white rounded-lg overflow-hidden shadow-lg w-11/12 max-w-sm">
         <div class="p-6">
-            <h2 class="text-lg font-bold mb-4">Confirm Logout</h2>
+            <h2 class="text-lg font-bold mb-4">Confirm Delete</h2>
             <p class="mb-6">Are you sure you want to delete this documentation?</p>
             <div class="flex justify-end">
                 <button id="cancelLogout1" class="px-4 py-2 bg-gray-200 rounded mr-2">Cancel</button>
@@ -21,7 +21,7 @@
 <div id="logoutModal2" class="hidden fixed inset-0 bg-black bg-opacity-70 z-50 flex items-center justify-center">
     <div class="bg-white rounded-lg overflow-hidden shadow-lg w-11/12 max-w-sm">
         <div class="p-6">
-            <h2 class="text-lg font-bold mb-4">Confirm Logout</h2>
+            <h2 class="text-lg font-bold mb-4">Confirm Delete</h2>
             <p class="mb-6">Are you sure you want to delete this resource?</p>
             <div class="flex justify-end">
                 <button id="cancelLogout2" class="px-4 py-2 bg-gray-200 rounded mr-2">Cancel</button>
